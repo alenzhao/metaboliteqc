17c4a43c2f57c0d5d7f395f238ea68424d4b748d
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2016-07-26 12:23:07 +0200

    Add bybyboxplot function.

--- R/metaboliteqc.R
+++ R/metaboliteqc.R
@@ -327,3 +327,34 @@ byboxplot <- function(mat, by, ylab, ylim = NULL) {
     box()
     invisible()
 }
+
+#' Draw boxplots for rectangular subsets of matrix
+#'
+#' @param mat Matrix
+#' @param by.x Factor to group columns of \code{mat} into subsets
+#' @param by.y Factor to group rows of \code{mat} into subsets
+#' @param main Title
+#' @param ylim y-axis range to use for all subplots
+#' @details The \code{by.x} argument has suffix "x" because the
+#'     subsets of columns that \code{by.x} creates will be drawn along
+#'     the x-axis.  Similarly, the \code{by.y} argument has suffix "y"
+#'     because the subsets of rows that \code{by.y} creates will be
+#'     drawn along the y-axis.
+#' @return Returns \code{NULL} invisibly.
+#' @export
+bybyboxplot <- function(mat, by.x, by.y, main = NULL, ylim = NULL) {
+    number_of_plots <- length(unique(by.y))
+    ds <- split.data.frame(mat, by.y)
+    if (is.null(main))
+        main <- "Boxplots"
+    par(mfrow = c(number_of_plots, 1),
+        oma = c(3, 0, 6, 0))
+    for (i in seq_along(ds)) {
+        byboxplot(ds[[i]], by.x, names(ds)[i], ylim = ylim)
+        if (i == 1L)
+            axis(3, at = seq_along(levels(by.x)), labels = levels(by.x), cex.axis = 1)
+    }
+    axis(1, at = seq_along(levels(by.x)), labels = levels(by.x), cex.axis = 1)
+    title(main, line = 4, outer = TRUE, cex.main = 1.5)
+    invisible()
+}
